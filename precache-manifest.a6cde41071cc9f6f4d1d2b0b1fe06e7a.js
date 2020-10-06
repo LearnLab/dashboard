861622b402839ebf81fdcc35113de533ab2672d4
Diego Castillo <61103323+LearnLab@users.noreply.github.com>
2020-10-06 11:18:06 -0500

    Updates

---
+++ precache-manifest.a6cde41071cc9f6f4d1d2b0b1fe06e7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681b992d0a37dbe5498f416c2ede84f4",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "f7bc2bae8f70822cead4",
+    "url": "/dashboard/static/css/2.546ed383.chunk.css"
+  },
+  {
+    "revision": "7cee3f116f7d92309899",
+    "url": "/dashboard/static/css/main.9f72999a.chunk.css"
+  },
+  {
+    "revision": "f7bc2bae8f70822cead4",
+    "url": "/dashboard/static/js/2.f3e06e46.chunk.js"
+  },
+  {
+    "revision": "656ea20e7001de4c6ac3ad4350aa0ffd",
+    "url": "/dashboard/static/js/2.f3e06e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cee3f116f7d92309899",
+    "url": "/dashboard/static/js/main.14a607a1.chunk.js"
+  },
+  {
+    "revision": "0ff1efcf7443a0f6c7e6",
+    "url": "/dashboard/static/js/runtime-main.9ddddbb6.js"
+  }
+]);
